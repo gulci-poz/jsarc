ab7b0748dc95ee42c8c301a714430463f82c3184
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2016-01-25 05:16:54 +0100

    2016-01-25 initial commit

---
+++ exarc01.js
@@ -0,0 +1,19 @@
+// https://developers.arcgis.com/javascript/
+
+/*
+require(["esri/map", "dojo/domReady!"], function (Map) {
+    var map = new Map("map", {
+        center: [-118, 34.5],
+        zoom: 8,
+        basemap: "topo"
+    });
+});
+*/
+
+require(["esri/map", "dojo/domReady!"], function(Map) {
+  var map = new Map("map", {
+    center: [-118, 34.5],
+    zoom: 8,
+    basemap: "topo"
+  });
+});
